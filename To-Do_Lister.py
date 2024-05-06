06b18c840f8950a4df092a1035f1682ef09a8c10
Emircan Çankal <emrcncnkl@gmail.com>
2024-05-06 21:05:37 +0300

    Add files via upload

---
+++ To-Do_Lister.py
@@ -0,0 +1,16 @@
+todo_list = []
+continue_list = 'y'
+while continue_list == 'y':
+    action = input("Enter 'a' to add a task and 'd' to delete a task:")
+    task = 0
+    if action == "a":
+        task = input("Enter the task you want to add:")
+        todo_list.append(task)
+    elif action == "d":
+        task = input("Enter the task you want to delete:")
+        if task in todo_list:
+            todo_list.remove(task)
+    print(todo_list)
+    continue_list = input("Do you want to add another item to your list? (y/n)")
+
+
